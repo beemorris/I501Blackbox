f90ec2517c8aef667246e775aa124f881edc5296
beemorris <morrisrebeccar@gmail.com>
2018-11-23 23:16:23 -0600

    it still does not q3

--- getq3.py
+++ getq3.py
@@ -12,17 +12,16 @@ import numpy as np
 import os
 
 def get_q3():
-	path = '/Users/bee/Library/Mobile Documents/com~apple~CloudDocs/IU/Fall2018/I501/Blackbox/I501Blackbox/'
-
-	for filename in os.listdir(path):
-		if filename.endswith('_int1.txt'):
-			full_matrix = np.loadtxt(filename, delimiter=',')
+	path = '/Users/bee/Library/Mobile Documents/com~apple~CloudDocs/IU/Fall2018/I501/Blackbox/I501Blackbox/Simulations/'
+	for file in os.listdir(path):
+		if file.endswith('.txt'):
+			full_matrix = np.loadtxt(file, delimiter=',')
 			quad1_matrix = full_matrix[0:10, 10:20]  # top right
 			quad2_matrix = full_matrix[0:10, 0:10]  # top left
 			quad3_matrix = full_matrix[10:20, 0:10]  # bottom left
 			quad4_matrix = full_matrix[10:20, 10:20]  # bottom right
-			print(quad3_matrix)
 
+			print(full_matrix)
 
 get_q3()
 
