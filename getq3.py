b1bd342a139c563ebfe42988bbecd75c9208d3eb
beemorris <morrisrebeccar@gmail.com>
2018-12-02 12:18:19 -0500

    fixed it for my laptop

--- getq3.py
+++ getq3.py
@@ -12,25 +12,26 @@ import numpy as np
 import os
 
 def get_q3(sim):
-	#path = '/Users/bee/Library/Mobile Documents/com~apple~CloudDocs/IU/Fall2018/I501/Blackbox/I501Blackbox/Simulations' #For Becca's computer
-    #path = 'C:/Users/kyrie/OneDrive/Documents/GitHub/I501Blackbox/Simulations/'  # for Kyrie's Laptop
-    path = 'C:/Users/kyrie/OneDrive/Documents/GitHub/I501Blackbox/Simulation 3 (4447 Steps)/'  # for Kyrie's Laptop
-    
-	#for file in sorted(os.listdir(path), key=lambda x: int(x.replace('_int1.txt', '').replace('sim3_step', ''))):
-    for file in sorted(os.listdir(path), key=lambda x: int(x.replace('_int1.txt', '').replace('sim' + str(sim) + '_step', ''))):
-        if file.endswith('.txt'):
-            full_matrix = np.loadtxt(path + file, delimiter=',')
-            #quad1_matrix = full_matrix[0:10, 10:20]  # top right
-            quad3_matrix = full_matrix[10:20, 0:10]  # bottom left
-            #quad4_matrix = full_matrix[10:20, 10:20]  # bottom right
-            print(file)
-            print(quad3_matrix)  # Prints the quad 3 matrix for each file (might need to do something different to visualize better)
+	path = '/Users/bee/I501Blackbox/Simulation 3 (4447 Steps)/' #For Becca's computer
+	#path = 'C:/Users/kyrie/OneDrive/Documents/GitHub/I501Blackbox/Simulations/'  # for Kyrie's Laptop
+	#path = 'C:/Users/kyrie/OneDrive/Documents/GitHub/I501Blackbox/Simulation 3 (4447 Steps)/'  # for Kyrie's Laptop
+
+	for file in sorted(os.listdir(path), key=lambda x: int(x.replace('_int1.txt', '').replace('sim3_step', ''))):
+	#for file in sorted(os.listdir(path), key=lambda x: int(x.replace('_int1.txt', '').replace('sim' + str(sim) + '_step', ''))):
+		if file.endswith('.txt'):
+			full_matrix = np.loadtxt(path + file, delimiter=',')
+			#quad1_matrix = full_matrix[0:10, 10:20]  # top right
+			quad3_matrix = full_matrix[10:20, 0:10]  # bottom left
+			#quad4_matrix = full_matrix[10:20, 10:20]  # bottom right
+			print(file)
+			print(quad3_matrix)  # Prints the quad 3 matrix for each file (might need to do something different to visualize better)
+
 
 '''
 sim_num = [1,2,3] #
 for elem in range(0,len(sim_num)):
-    print(str(sim_num[elem]))
-    get_q3(sim_num[elem])'''
+	print(str(sim_num[elem]))
+	get_q3(sim_num[elem])'''
 
 
 get_q3(3)
